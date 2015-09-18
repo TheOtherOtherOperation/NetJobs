06821c29eea9e74f29cf4b692abfd0cf7380e521
Ramón A. Lovato <cubevato@gmail.com>
2015-09-18 13:06:36 -0600

    Shebang updated to python3

--- NetJobs.py
+++ NetJobs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # ############################################################################ #
 # NetJobs - a network job synchronizer.                                        #
